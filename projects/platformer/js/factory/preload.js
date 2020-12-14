537542b7bf28b25c2c3954942d6ad40032667884
darousej93 <jacob.darouse@gmail.com>
2020-12-14 00:33:18 +0000

    trying to make the platformer work without any files other than index so that I can use it for my freshmen

--- projects/platformer/js/factory/preload.js
+++ projects/platformer/js/factory/preload.js
@@ -5,14 +5,14 @@
     
     // TODO : Load config for url //
     opspark.preload = function (game) {
-        game.load.image('cannon', './asset/cannon.png');
-        game.load.image('projectile', './asset/projectile.png');
-        game.load.image('platform', './asset/platform.png');
-        game.load.image('db', './asset/collectable/database.png');
-        game.load.image('steve', './asset/collectable/steve-head.png');
-        game.load.image('grace', './asset/collectable/grace-head.png');
-        game.load.image('kennedi', './asset/collectable/kennedi-head.png');
-        game.load.image('max', './asset/collectable/max-head.png');
-        game.load.atlas('halle', './asset/halle/phaser-json-array/halle.png', './asset/halle/phaser-json-array/halle.json');
+        game.load.image('cannon', 'https://benspectoros.github.io/projects/platformer/asset/cannon.png');
+        game.load.image('projectile', 'https://benspectoros.github.io/projects/platformer/asset/projectile.png');
+        game.load.image('platform', 'https://benspectoros.github.io/projects/platformer/asset/platform.png');
+        game.load.image('db', 'https://benspectoros.github.io/projects/platformer/asset/collectable/database.png');
+        game.load.image('steve', 'https://benspectoros.github.io/projects/platformer/asset/collectable/steve-head.png');
+        game.load.image('grace', 'https://benspectoros.github.io/projects/platformer/asset/collectable/grace-head.png');
+        game.load.image('kennedi', 'https://benspectoros.github.io/projects/platformer/asset/collectable/kennedi-head.png');
+        game.load.image('max', 'https://benspectoros.github.io/projects/platformer/asset/collectable/max-head.png');
+        game.load.atlas('halle', 'https://benspectoros.github.io/projects/platformer/asset/halle/phaser-json-array/halle.png', 'https://benspectoros.github.io/projects/platformer/asset/halle/phaser-json-array/halle.json');
     };
 })(window);
